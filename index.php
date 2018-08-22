cfc2dc97c658034929993484bc612930590ceffd
massa <massafranck@gmail.com>
2018-08-22 21:57:11 +0200

    corrigé

--- index.php
+++ index.php
@@ -1,19 +1,31 @@
 <!DOCTYPE html>
 <html lang="fr">
-  <head>
-    <meta charset="utf-8" />
-    <title>Exercice5</title>
-  </head>
-  <body>
-    <!--Création d'un formulaire qui redirige vers la page index.php avec la méthode GET-->
-    <form action="index.php" method="get">
-      <select name="civility">
-        <option value="Mr">Mr</option>
-        <option value="Mrs">Mme</option>
-      </select>
-      <p><label for="firstName">Prénom :</label><input type="text" name="firstName" id="firstName" /><br/></p>
-      <p><label for="lastName">Nom :</label><input type="text" name="lastName" id="lastName" /></p>
-      <input type="submit" />
-    </form>
-  </body>
+    <head>
+        <meta charset="utf-8" />
+        <title>p7exo5</title>
+    </head>
+    <body>
+        <form action="index.php" method="POST">
+            <label for="civility">Civilité</label>
+            <select id="civility" name="civility">
+                <!-- Permet de ne pas oublier de selectionner la civilité --> 
+                <option selected disabled>Veuillez selectionner une option</option>
+                <option value="M">M</option>
+                <option value="Mme">Mme</option>
+            </select>
+            <label for="lastname">Nom</label><input type="text" name="lastname" placeholder="Nom" />
+            <label for="firstname">Prénom</label><input type="text" name="firstname" placeholder="Prénom" />
+            <input type="submit" name="submit" value="soumettre" />
+        </form>
+        <p>
+            <?php
+            //On vérifie que les valeurs existent et on affiche les valeurs
+            if (!empty($_POST['civility']) && !empty($_POST['lastname']) && !empty($_POST['firstname'])) {
+                echo $_POST['civility'] . ' ' . $_POST['lastname'] . ' ' . $_POST['firstname'];
+            } else {
+                echo 'veuillez entrée nom et prénom';
+            }
+            ?>
+        </p>
+    </body>
 </html>
